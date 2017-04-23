b0414450881a8266cf677d6ebbb49a98ec8468f2
DESKTOP-9CKQ8II\tolea <tolea.olaru92@gmail.com>
2017-04-23 21:16:53 +0300

    class2 class3

---
+++ WebApp/WebApp/TestClasses/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.TestClasses {
+	public class Class2 {
+
+		public void Method2() {
+		}
+	}
+}
